403aa72e875def75aa392cc0d6ca17d8ca1888a6
Ryan LaBouve <imryanlabouve@gmail.com>
2022-05-15 18:01:44 -0500

    science

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,15 @@
+import SimpleImageSlider from "react-simple-image-slider";
+import useWindowSize from "~/hooks/use-window-size";
+
+interface Image {
+  original: string;
+  thumbnail: string;
+}
 interface Service {
   heading: string;
   description: string;
   background: string;
-  images: string[];
+  images: Image[];
 }
 
 const services: Service[] = [
@@ -11,27 +18,63 @@ const services: Service[] = [
     description:
       "Removing features of an existing space or strucutre. Examples: when one commercial space moves out and a new tennent requires something different ",
     background: "/images/selective-demolition-1.jpg",
-    images: [],
+    images: [
+      {
+        original: "/images/selective-demolition/1-1200.jpg",
+        thumbnail: "/images/selective-demolition/1-600-thumbnail.jpg",
+      },
+      {
+        original: "/images/selective-demolition/2-1200.jpg",
+        thumbnail: "/images/selective-demolition/2-600-thumbnail.jpg",
+      },
+    ],
   },
   {
     heading: "Deconstruction",
     description: "Breaking down a strucutre to salvage the underlying material",
     background: "/images/deconstruction-bg-1.jpg",
-    images: [],
+    images: [
+      {
+        original: "/images/deconstruction/1-1200.jpg",
+        thumbnail: "/images/deconstruction/1-600-thumbnail.jpg",
+      },
+      {
+        original: "/images/deconstruction/2-1200.jpg",
+        thumbnail: "/images/deconstruction/2-600-thumbnail.jpg",
+      },
+    ],
   },
   {
     heading: "Structural Demolition",
     description:
       "Removal of whole structures. Examples: removing an existing home to rebuild, removing commercial strucutres to rebuild",
     background: "/images/total-demolition-bg-4.jpg",
-    images: [],
+    images: [
+      {
+        original: "/images/structural-demolition/1-1200.jpg",
+        thumbnail: "/images/strucrtual-demolition/1-600-thumbnail.jpg",
+      },
+      {
+        original: "/images/structural-demolition/2-1200.jpg",
+        thumbnail: "/images/structural-demolition/2-600-thumbnail.jpg",
+      },
+    ],
   },
   {
     heading: "Concrete Removal",
     description:
       "From removing large runs of parking lot to creating new runs in foundations for plumbing",
     background: "/images/concrete-removal-bg-1.jpg",
-    images: [],
+    images: [
+      {
+        original: "/images/concrete-removal/1-1200.jpg",
+        thumbnail: "/images/concrete-removal/1-600-thumbnail.jpg",
+      },
+      {
+        original: "/images/concrete-removal/2-1200.jpg",
+        thumbnail: "/images/concrete-removal/2-600-thumbnail.jpg",
+      },
+    ],
   },
 
   {
@@ -39,10 +82,23 @@ const services: Service[] = [
     description:
       "When storms come'a'knocking we go a rocking. Goodstar parterns with various diaster relief organizations to volunteer.",
     background: "/images/disaster-recovery-bg-1.jpg",
-    images: [],
+    images: [
+      {
+        original: "/images/dr/1-1200.jpg",
+        thumbnail: "/images/dr/1-600-thumbnail.jpg",
+      },
+      {
+        original: "/images/dr/2-1200.jpg",
+        thumbnail: "/images/dr/2-600-thumbnail.jpg",
+      },
+    ],
   },
 ];
 export default function Index() {
+  let windowSize = useWindowSize();
+  let w = windowSize.width > 800 ? 600 : 300;
+  let h = windowSize.width > 800 ? 400 : 200;
+
   return (
     <div>
       {/*Header*/}
@@ -123,16 +179,26 @@ export default function Index() {
             style={{
               backgroundImage: `url(${background})`,
             }}
-            className="bg-gray-300 py-20 bg-cover text-white"
+            className="bg-gray-300 py-20 bg-cover text-white "
             key={heading}
           >
-            <div className="flex  max-w-7xl m-auto px-4 py-8">
-              <div className="grid grid-cols-2 w-full gap-12">
-                <div>
-                  <div className="text-2xl">{heading}</div>
-                  <div>{description}</div>
+            <div className="flex  max-w-7xl m-auto px-4 py-4">
+              <div className="grid sm:grid-cols-1 md:grid-cols-2 w-full gap-12">
+                <div className="flex items-center">
+                  <div>
+                    <div className="text-4xl mb-2">{heading}</div>
+                    <div className="text-lg">{description}</div>
+                  </div>
                 </div>
-                <div>images</div>
+                <div>
+                  <SimpleImageSlider
+                    width={w}
+                    height={h}
+                    images={images.map((i) => i.original)}
+                    showBullets={true}
+                    showNavs={true}
+                  />
+                </div>
               </div>
             </div>
           </div>
